0b427e28a019522283339ba13188fc86281798c5
Huy Nguyen <huy.m.nguyen@live.com>
2019-10-09 00:19:42 -0700

    Add css for footer. Add import and export in index.js and footer.js.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+    <footer className='footer'>
+        <div className='footer-bottom'>
+            Clicky Game!
+            <img alt='react' src='../../../logo.svg' />
+        </div>
+    </footer>
+);
+
+export default Footer;
