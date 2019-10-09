b0bd62134f166ed0bef0e8b24d46c07380ac6789
Huy Nguyen <huy.m.nguyen@live.com>
2019-10-08 22:57:47 -0700

    Moved onclick to item.js. Add export from ./Item in index.js

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import './Item.css';
-
-const Item = ({name, image, onClick}) =>
- <img
-    className='click-item'
-    src={image}
-    alt={image}
-    name={name}
-    onClick={ () => onClick(name) }
-/>;
-
-export default Item;
+export { default } from './Item';
